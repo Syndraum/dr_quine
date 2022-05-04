97d67c5251074714b2992c9d7f3f74127b45c1fb
roalvare <roalvare@student.42.fr>
2022-05-04 17:46:27 +0200

    feat: smaller quine

--- Colleen/colleen.c
+++ Colleen/colleen.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	char *s1="#include <stdio.h>%c%cint main() {%c";
-	char *s2="%cchar *s1=%c%s%c;%c%cchar *s2=%c%s%c;%c%cprintf(s1, 10, 10, 10);%c%cprintf(s2, 9, 34, s1, 34, 10, 9, 34, s2, 34, 10, 9, 10, 9, 10);%c}";
-	printf(s1, 10, 10, 10);
-	printf(s2, 9, 34, s1, 34, 10, 9, 34, s2, 34, 10, 9, 10, 9, 10);
+	char *s1="#include <stdio.h>%c%cint main() {%c%cchar *s1=%c%s%c;%c%cprintf(s1, 10, 10, 10, 9, 34, s1, 34, 10, 9, 10);%c}";
+	printf(s1, 10, 10, 10, 9, 34, s1, 34, 10, 9, 10);
 }
