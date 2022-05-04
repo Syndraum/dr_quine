4767a3bc686bba854ff415691134793534b89ff0
roalvare <roalvare@student.42.fr>
2022-05-04 18:29:01 +0200

    feat: echo function

--- Colleen/colleen.c
+++ Colleen/colleen.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+void echo(char *s1) {
+	printf(s1, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);
+}
+
 /*
 	This program will print its own source when run.
 */
 int main() {
 	// Begin program
-	char *s1="#include <stdio.h>%c%c/*%c%cThis program will print its own source when run.%c*/%cint main() {%c%c// Begin program%c%cchar *s1=%c%s%c;%c%cprintf(s1, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);%c}";
-	printf(s1, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);
+	char *s1="#include <stdio.h>%c%cvoid echo(char *s1) {%c%cprintf(s1, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);%c}%c%c/*%c%cThis program will print its own source when run.%c*/%cint main() {%c%c// Begin program%c%cchar *s1=%c%s%c;%c%cecho(s1);%c}";
+	echo(s1);
 }
