23809df33755d691c819058526532b0803a40572
roalvare <roalvare@student.42.fr>
2022-05-04 18:11:56 +0200

    feat: add comment in colleen

--- Colleen/colleen.c
+++ Colleen/colleen.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/*
+	This program will print its own source when run.
+*/
 int main() {
-	char *s1="#include <stdio.h>%c%cint main() {%c%cchar *s1=%c%s%c;%c%cprintf(s1, 10, 10, 10, 9, 34, s1, 34, 10, 9, 10);%c}";
-	printf(s1, 10, 10, 10, 9, 34, s1, 34, 10, 9, 10);
+	// Begin program
+	char *s1="#include <stdio.h>%c%c/*%c%cThis program will print its own source when run.%c*/%cint main() {%c%c// Begin program%c%cchar *s1=%c%s%c;%c%cprintf(s1, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);%c}";
+	printf(s1, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);
 }
