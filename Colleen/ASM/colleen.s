ac8aca6c1d54c1eaaf5fdb0f94837048a2d09ed6
roalvare <roalvare@student.42.fr>
2022-05-05 17:23:39 +0200

    fix: memory alignment

--- Colleen/ASM/colleen.s
+++ Colleen/ASM/colleen.s
@@ -2,13 +2,15 @@ global main
 section .text
 extern printf
 main:
+  sub  rsp, 8
   mov  rdi, s1
   mov  rsi, 10
   mov  rdx, 34
   mov  rcx, s1
-  mov  eax, 0
+  xor  rax, rax
   call printf
   xor  rax, rax
+  add  rsp, 8
   ret
-section .data
-s1: db "global main%1$csection .text%1$cextern printf%1$cmain:%1$c  mov  rdi, s1%1$c  mov  rsi, 10%1$c  mov  rdx, 34%1$c  mov  rcx, s1%1$c  mov  eax, 0%1$c  call printf%1$c  xor  rax, rax%1$c  ret%1$csection .data%1$cs1: db %2$c%3$s%2$c"
+section .rodata
+s1: db "global main%1$csection .text%1$cextern printf%1$cmain:%1$c  sub  rsp, 8%1$c  mov  rdi, s1%1$c  mov  rsi, 10%1$c  mov  rdx, 34%1$c  mov  rcx, s1%1$c  xor  rax, rax%1$c  call printf%1$c  xor  rax, rax%1$c  add  rsp, 8%1$c  ret%1$csection .rodata%1$cs1: db %2$c%3$s%2$c"
