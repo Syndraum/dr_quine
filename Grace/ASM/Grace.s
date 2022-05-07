f3805ea83266f3ab244c8cf7f7976cbd697f2fa0
roalvare <roalvare@student.42.fr>
2022-05-07 16:03:03 +0200

    fix<Grace>: only 3 define

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,5 +1,14 @@
+%macro FNAME 0
+name: db "Grace_kid.s", 0
+%endmacro
+%macro CODE 0
+s1: db "%%macro FNAME 0%1$cname: db %2$cGrace_kid.s%2$c, 0%1$c%%endmacro%1$c%%macro CODE 0%1$cs1: db %2$c%3$s%2$c, 0%1$c%%endmacro%1$c%%macro FT 0%1$cglobal main%1$csection .rodata%1$cFNAME%1$cCODE%1$csection .text%1$cextern dprintf%1$c%4$c This program will print its own source when run.%1$cmain:%1$cmov rax, 2%1$cmov rdi, name%1$cmov rsi, 577%1$cmov rdx, 436%1$csyscall%1$cmov r14, rax%1$csub rsp, 8%1$cmov rdi, r14%1$cmov rsi, s1%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, s1%1$cmov r9, 59%1$ccall dprintf%1$cadd rsp, 8%1$cmov rdi, r14%1$cmov rax, 3%1$csyscall%1$cret%1$c%%endmacro%1$c%1$cFT", 0
+%endmacro
 %macro FT 0
 global main
+section .rodata
+FNAME
+CODE
 section .text
 extern dprintf
 ; This program will print its own source when run.
@@ -23,9 +32,6 @@ mov rdi, r14
 mov rax, 3
 syscall
 ret
-section .rodata
-name: db "Grace_kid.s", 0
-s1: db "%%macro FT 0%1$cglobal main%1$csection .text%1$cextern dprintf%1$c%4$c This program will print its own source when run.%1$cmain:%1$cmov rax, 2%1$cmov rdi, name%1$cmov rsi, 577%1$cmov rdx, 436%1$csyscall%1$cmov r14, rax%1$csub rsp, 8%1$cmov rdi, r14%1$cmov rsi, s1%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, s1%1$cmov r9, 59%1$ccall dprintf%1$cadd rsp, 8%1$cmov rdi, r14%1$cmov rax, 3%1$csyscall%1$cret%1$csection .rodata%1$cname: db %2$cGrace_kid.s%2$c, 0%1$cs1: db %2$c%3$s%2$c, 0%1$c%%endmacro%1$c%1$cFT", 0
 %endmacro
 
 FT
--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,8 +1,6 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#define s1 "#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdio.h>%1$c#define s1 %2$c%3$s%2$c%1$c#define FT()int main(){int fd=open(%2$cGrace_kid.c%2$c, 577, 436);dprintf(fd, s1, 10, 34, s1);close(fd);}%1$c/*%1$c    This program will print its own source when run.%1$c*/%1$cFT()"
-#define FT()int main(){int fd=open("Grace_kid.c", 577, 436);dprintf(fd, s1, 10, 34, s1);close(fd);}
+#define FNAME "Grace_kid.c"
+#define s1 "#define FNAME %2$cGrace_kid.c%2$c%1$c#define s1 %2$c%3$s%2$c%1$c#define FT(x)int main(){int open(const char*, int, int); int dprintf(int, const char *, ...);int close(int);int fd=open(FNAME, 577, 436);dprintf(fd, s1, 10, 34, s1);close(fd);}%1$c/*%1$c    This program will print its own source when run.%1$c*/%1$cFT()"
+#define FT(x)int main(){int open(const char*, int, int); int dprintf(int, const char *, ...);int close(int);int fd=open(FNAME, 577, 436);dprintf(fd, s1, 10, 34, s1);close(fd);}
 /*
     This program will print its own source when run.
 */
