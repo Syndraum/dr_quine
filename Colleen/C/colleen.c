b13c6896d6ff21fe77463661add74a7a2728c8eb
roalvare <roalvare@student.42.fr>
2022-05-07 13:07:09 +0200

    feat: better c solution

--- Colleen/C/colleen.c
+++ Colleen/C/colleen.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
-void echo(char *s1) {
-	printf(s1, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);
+void echo(char *s1){
+printf(s1, 10, 34, s1);
 }
-
 /*
-	This program will print its own source when run.
+    This program will print its own source when run.
 */
-int main() {
-	// Begin program
-	char *s1="#include <stdio.h>%c%cvoid echo(char *s1) {%c%cprintf(s1, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 10, 10, 9, 10, 9, 34, s1, 34, 10, 9, 10);%c}%c%c/*%c%cThis program will print its own source when run.%c*/%cint main() {%c%c// Begin program%c%cchar *s1=%c%s%c;%c%cecho(s1);%c}";
-	echo(s1);
+int main(){
+/*
+    Begin program
+*/
+char *s1="#include <stdio.h>%1$cvoid echo(char *s1){%1$cprintf(s1, 10, 34, s1);%1$c}%1$c/*%1$c    This program will print its own source when run.%1$c*/%1$cint main(){%c/*%1$c    Begin program%1$c*/%1$cchar *s1=%2$c%3$s%2$c;%1$cecho(s1);%1$c}";
+echo(s1);
 }
--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,8 +1,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#define s1 "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#define s1 %c%s%c%c#define FT()int main(){int fd = open(%cGrace_kid.c%c, 577, 436);dprintf(fd, s1, 10, 10, 10, 34, s1, 34, 10, 34, 34, 10, 10, 10, 10);close(fd);}%c/*%c    This program will print its own source when run.%c*/%cFT()"
-#define FT()int main(){int fd = open("Grace_kid.c", 577, 436);dprintf(fd, s1, 10, 10, 10, 34, s1, 34, 10, 34, 34, 10, 10, 10, 10);close(fd);}
+#define s1 "#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdio.h>%1$c#define s1 %2$c%3$s%2$c%1$c#define FT()int main(){int fd=open(%2$cGrace_kid.c%2$c, 577, 436);dprintf(fd, s1, 10, 34, s1);close(fd);}%1$c/*%1$c    This program will print its own source when run.%1$c*/%1$cFT()"
+#define FT()int main(){int fd=open("Grace_kid.c", 577, 436);dprintf(fd, s1, 10, 34, s1);close(fd);}
 /*
     This program will print its own source when run.
 */
